7375aa0779a489d0763f106682959508a9c4a899
yasindu-dilshan <yasindu.18@cse.mrt.ac.lk>
2023-06-17 16:58:46 +0530

    Edit Info table

--- components/table-data.js
+++ components/table-data.js
@@ -2,56 +2,40 @@ import { Box, Table, Thead, Tbody, Tr, Th, Td, useColorModeValue } from '@chakra
 import React from 'react';
 
 const TableComponent = ({ data }) => {
-    const tableBgColor = useColorModeValue("gray.300", "gray.700"); // Make the table color darker
-    const tableBorderColor = useColorModeValue("black", "white");
-    const headerBgColor = useColorModeValue("gray.400", "gray.900"); // Darker background for header
-    const roundedCorner = "lg";
+    const headerBgColor = useColorModeValue("gray.600", "gray.900"); // Darker background for header
 
     return (
         <Box overflowX="auto" p="4">
-            <Table
-                variant="simple"
-                bg={tableBgColor}
-                borderColor={tableBorderColor}
-                borderCollapse="separate"
-                borderSpacing="10rem" // Add space between rows and columns
-                borderRadius={roundedCorner} // Add rounded corners to table
-            >
+            <Table>
                 <Thead bg={headerBgColor}>
                     <Tr>
-                        <Th color={useColorModeValue("gray.700", "gray.800")} py="5">Currency Name</Th>
-                        <Th color={useColorModeValue("gray.700", "gray.800")} py="5">Currency Symbol</Th>
-                        <Th color={useColorModeValue("gray.700", "gray.800")} py="5" px="10">Marketplace Name</Th>
+                        <Th color={useColorModeValue("gray.100", "gray.100")} py="5">Currency Name</Th>
+                        <Th color={useColorModeValue("gray.100", "gray.100")} py="5">Currency Symbol</Th>
+                        <Th color={useColorModeValue("gray.100", "gray.100")} py="5" px="10">Currency Exchange Name</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
-                    {data.map((currency) => (
-                        <Tr key={currency.currencySymbol}>
+                    {data.map((currency, index) => (
+                        <Tr
+                            key={currency.currencySymbol}
+                            bg={index % 2 === 0 ? "gray.400" : "gray.300"} // Alternating row colors
+                        >
                             <Td
-                                color={useColorModeValue("gray.800", "white")}
-                                borderRadius={roundedCorner} // Add rounded corners
-                                borderTopColor={tableBorderColor}
-                                borderTopWidth="1.5px"
+                                borderTopWidth="4px"
                                 px="16"
                                 py="4" // Increase vertical padding
                             >
                                 {currency.currencyName}
                             </Td>
                             <Td
-                                color={useColorModeValue("gray.800", "white")}
-                                borderRadius={roundedCorner}
-                                borderTopColor={tableBorderColor}
-                                borderTopWidth="1.5px"
+                                borderTopWidth="4px"
                                 px="16"
                                 py="4"
                             >
                                 {currency.currencySymbol}
                             </Td>
                             <Td
-                                color={useColorModeValue("gray.800", "white")}
-                                borderRadius={roundedCorner}
-                                borderTopColor={tableBorderColor}
-                                borderTopWidth="1.5px"
+                                borderTopWidth="4px"
                                 px="16"
                                 py="4"
                             >
@@ -67,3 +51,74 @@ const TableComponent = ({ data }) => {
 
 export default TableComponent;
 
+
+
+// import { Box, Table, Thead, Tbody, Tr, Th, Td, useColorModeValue } from '@chakra-ui/react';
+// import React from 'react';
+//
+// const TableComponent = ({ data }) => {
+//     const tableBgColor = useColorModeValue("gray.300", "gray.700"); // Make the table color darker
+//     const tableBorderColor = useColorModeValue("black", "white");
+//     const headerBgColor = useColorModeValue("gray.400", "gray.900"); // Darker background for header
+//     const roundedCorner = "lg";
+//
+//     return (
+//         <Box overflowX="auto" p="4">
+//             <Table
+//                 variant="simple"
+//                 bg={tableBgColor}
+//                 borderColor={tableBorderColor}
+//                 borderCollapse="separate"
+//                 borderSpacing="10rem" // Add space between rows and columns
+//                 borderRadius={roundedCorner} // Add rounded corners to table
+//             >
+//                 <Thead bg={headerBgColor}>
+//                     <Tr>
+//                         <Th color={useColorModeValue("gray.700", "gray.800")} py="5">Currency Name</Th>
+//                         <Th color={useColorModeValue("gray.700", "gray.800")} py="5">Currency Symbol</Th>
+//                         <Th color={useColorModeValue("gray.700", "gray.800")} py="5" px="10">Marketplace Name</Th>
+//                     </Tr>
+//                 </Thead>
+//                 <Tbody>
+//                     {data.map((currency) => (
+//                         <Tr key={currency.currencySymbol}>
+//                             <Td
+//                                 color={useColorModeValue("gray.800", "white")}
+//                                 borderRadius={roundedCorner} // Add rounded corners
+//                                 borderTopColor={tableBorderColor}
+//                                 borderTopWidth="1.5px"
+//                                 px="16"
+//                                 py="4" // Increase vertical padding
+//                             >
+//                                 {currency.currencyName}
+//                             </Td>
+//                             <Td
+//                                 color={useColorModeValue("gray.800", "white")}
+//                                 borderRadius={roundedCorner}
+//                                 borderTopColor={tableBorderColor}
+//                                 borderTopWidth="1.5px"
+//                                 px="16"
+//                                 py="4"
+//                             >
+//                                 {currency.currencySymbol}
+//                             </Td>
+//                             <Td
+//                                 color={useColorModeValue("gray.800", "white")}
+//                                 borderRadius={roundedCorner}
+//                                 borderTopColor={tableBorderColor}
+//                                 borderTopWidth="1.5px"
+//                                 px="16"
+//                                 py="4"
+//                             >
+//                                 {currency.stockExchangeName}
+//                             </Td>
+//                         </Tr>
+//                     ))}
+//                 </Tbody>
+//             </Table>
+//         </Box>
+//     );
+// };
+//
+// export default TableComponent;
+//
